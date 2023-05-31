4b02c0595e7b1dd48939d7ee9154c35d25b80aca
Mulia Sujiastuti Wahyono <muliasujiastuti@gmail.com>
2023-05-31 13:20:41 +0700

    add header

--- components/auth/Auth.tsx
+++ components/auth/Auth.tsx
@@ -70,7 +70,73 @@ const Auth: React.FC = () => {
             <Link href={'/'} passHref>
                 <img src={'/assets/logo.svg'} alt="logo" className="logo" />
             </Link>
-            
+            <AuthError error={error}>{error}</AuthError>
+            <AuthForm onSubmit={submitHandler}>
+                {isLoading && <LoadingSpinner asOverlay text="Wait for it..." />}
+                <h1>{isLoggingMode ? 'Login' : 'Sign Up'}</h1>
+                <AuthInputWrapper>
+                    <label id="email">
+                        <AuthInput
+                            placeholder="Email address"
+                            type="text"
+                            aria-label="email"
+                            {...register('email', {
+                                required: true,
+                                pattern:
+                                    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                            })}
+                        />
+                    </label>
+                    {errors.email && <ErrorMessage>{'Invalid email'}</ErrorMessage>}
+                </AuthInputWrapper>
+                <AuthInputWrapper>
+                    <label id="password">
+                        <AuthInput
+                            placeholder="Password"
+                            type="password"
+                            {...register('password', {
+                                required: true,
+                            })}
+                            aria-label="password"
+                        />
+                    </label>
+                    {errors.password && <ErrorMessage>{"Can't be empty"}</ErrorMessage>}
+                </AuthInputWrapper>
+                {!isLoggingMode && (
+                    <AuthInputWrapper>
+                        <label id="repeted-password">
+                            <AuthInput
+                                placeholder="Repeat password"
+                                type="password"
+                                {...register('password_repeate', {
+                                    validate: (value) => value === password.current,
+                                })}
+                                aria-label="password_repeate"
+                            />
+                        </label>
+                        {errors.password_repeate && (
+                            <ErrorMessage>{'The passwords do not match'}</ErrorMessage>
+                        )}
+                    </AuthInputWrapper>
+                )}
+                <Button>
+                    {isLoggingMode ? 'Login to your account' : 'Create an account'}
+                </Button>
+                <p>
+                    {isLoggingMode
+                        ? 'Don’t have an account?'
+                        : 'Already have an account?'}{' '}
+                    <span
+                        onClick={() => dispatch(authActions.toggleLogin())}
+                        aria-label="login-toggle"
+                    >
+                        {isLoggingMode ? 'Sign Up' : 'Login'}
+                    </span>
+                </p>
+            </AuthForm>
         </AuthWrapper>
     )
-}
+};
+
+
+export default Auth;
